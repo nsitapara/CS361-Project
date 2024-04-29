a80de75b434e1590d3358b3fa35f897778de8540
nsitapara <nsitapara@gmail.com>
2024-04-28 21:47:07 -0700

    update footer

--- costcrew/components/HomeButton.tsx
+++ costcrew/components/HomeButton.tsx
@@ -1,9 +1,15 @@
+import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 
-export default function HomeButton() {
+export default async function HomeButton() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <Link
-      href="/"
+      href={user ? "/dashboard" : "/"}
       className="py-2 px-3 mr-4 flex rounded-md no-underline bg-emerald-700 hover:bg-emerald-800"
     >
       CostCrew
