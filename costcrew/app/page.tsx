b4cf795438c82cd390278969be42da0427df851d
nsitapara <nsitapara@gmail.com>
2024-04-28 22:40:45 -0700

    Update page.tsx

--- costcrew/app/page.tsx
+++ costcrew/app/page.tsx
@@ -22,11 +22,10 @@ export default async function Index() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+      <div className="animate-in flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        <main>
+          <h2>Next steps</h2>
         </main>
       </div>
     </div>
